991e19d2e371a020f66b2c4b7fd0cdd9f2efdafe
Krystian Walus <Krystian.Walus@mail.bcu.ac.uk>
2023-10-22 06:25:53 +0100

    Added accelerometer controls
    Tested the accelerometer afterwards and it works

--- Game Prototype/Assets/Scripts/PlayerMovement.cs
+++ Game Prototype/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,32 @@ public class PlayerMovement : MonoBehaviour
     public float dodgeSpeed = 5;
     public float rollSpeed = 5;
 
+    public enum MobileHorizMovement
+    {
+        Accelerometer,
+        ScreenTouch
+    }
+
+    public MobileHorizMovement horizMovement = MobileHorizMovement.Accelerometer;
+
+    private float CalculateMovement(Vector3 pixelPos)
+    {
+        var worldPos = Camera.main.ScreenToViewportPoint(pixelPos);
+
+        float xMove = 0;
+
+        if (worldPos.x < 0.5f)
+        {
+            xMove = -1;
+        }
+        else
+        {
+            xMove = 1;
+        }
+
+        return xMove * dodgeSpeed;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,15 +50,43 @@ public class PlayerMovement : MonoBehaviour
         //float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         //ch.Move(new Vector3(x, 0, moveSpeed * Time.deltaTime));
         var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed;
-        rb.AddForce(0, 0, rollSpeed * Time.deltaTime);
 
-        if (Input.GetKey("d"))
+        #if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
+
+        horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed;
+
+        if (Input.GetMouseButton(0))
         {
-            rb.AddForce(horizontalSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            horizontalSpeed = CalculateMovement(Input.mousePosition);
         }
-        if (Input.GetKey("a"))
+
+        #elif UNITY_IOS || UNITY_ANDROID
+
+        if(horizMovement == MobileHorizMovement.Accelerometer)
         {
-            rb.AddForce(horizontalSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            horizontalSpeed = Input.acceleration.x * dodgeSpeed;
         }
+
+        if (Input.touchCount > 0)
+        {
+            if (horizMovement == MobileHorizMovement.ScreenTouch)
+            {
+                Touch touch = Input.touches[0];
+                horizontalSpeed = CalculateMovement(touch.position);
+            }
+            
+        }
+        #endif
+
+        rb.AddForce(horizontalSpeed * Time.deltaTime, 0, rollSpeed * Time.deltaTime);
+
+        //if (Input.GetKey("d"))
+        //{
+        //    rb.AddForce(horizontalSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        //}
+        //if (Input.GetKey("a"))
+        //{
+        //    rb.AddForce(horizontalSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        //}
     }
 }
