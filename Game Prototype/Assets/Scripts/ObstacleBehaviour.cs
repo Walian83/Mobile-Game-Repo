2c56775c7450cbffe39548730d79f4f73f2b136a
Krystian Walus <91696197+Walian83@users.noreply.github.com>
2024-01-06 18:29:03 +0100

    Implemented interstitial ads

--- Game Prototype/Assets/Scripts/ObstacleBehaviour.cs
+++ Game Prototype/Assets/Scripts/ObstacleBehaviour.cs
@@ -6,10 +6,9 @@ using UnityEngine.SceneManagement;
 public class ObstacleBehaviour : MonoBehaviour
 {
     // How long to wait before restarting the game
-    public float waitTime = 2.0f;
+    public float waitTime = 1.0f;
     public Score score;
     public PlayerMovement movement;
-    public InterstitialAd ad;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -28,7 +27,7 @@ public class ObstacleBehaviour : MonoBehaviour
     {
         // Reloads the current scene
         SceneManager.LoadScene("Main Menu");
-        ad.ShowAd();
+        AdManager.instance.interstitialAds.ShowAd();
     }
 
     // Start is called before the first frame update
@@ -40,6 +39,6 @@ public class ObstacleBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+ 
     }
 }
