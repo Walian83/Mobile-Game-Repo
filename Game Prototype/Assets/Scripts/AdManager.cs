33a366ba3742539f3131fba2b29fcc5b90b33251
Krystian Walus <91696197+Walian83@users.noreply.github.com>
2024-01-11 15:49:03 +0100

    Revert "Revert "Finished the documentation, added code comments""
    This reverts commit 20cd75e36d0205237d9d6ee85cca1360b8292832.

--- Game Prototype/Assets/Scripts/AdManager.cs
+++ Game Prototype/Assets/Scripts/AdManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AdManager : MonoBehaviour
 {
+    // Variables for referencing the ad controller (initializing script) and ad types
     public UnityAdController controller;
     public BannerAd bannerAds;
     public InterstitialAd interstitialAds;
@@ -18,9 +19,12 @@ public class AdManager : MonoBehaviour
             return;
         }
         instance = this;
+        // Do not destroy the AdManager GameObject when loading new scenes
         DontDestroyOnLoad(gameObject);
 
+        // Load the banner ad when the game starts
         bannerAds.LoadBanner();
+        // Load the interstitial ad when the game starts
         interstitialAds.LoadAd();
     }
 }
